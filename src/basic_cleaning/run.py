72b2367430c241aa6a77dae9ba478856ea491c04
ryan-dutra <rdutra.d.abreu@gmail.com>
2024-06-05 08:28:59 -0300

    Fix code to new release

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -35,7 +35,7 @@ def go(args):
     df['last_review'] = pd.to_datetime(df['last_review'])
 
     # Drop rows in the dataset that are not in the proper geolocation
-    # ---
+    # ----
     idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
     df = df[idx].copy()
 
